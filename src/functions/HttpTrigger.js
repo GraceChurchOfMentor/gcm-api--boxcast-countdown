b5c6a69d235ff0bc4de22db2a16ee6e62e544d5f
Nick Bair <njbair@gmail.com>
2023-10-11 16:34:01 -0400

    initial commit

---
+++ src/functions/HttpTrigger.js
@@ -0,0 +1,46 @@
+const { app } = require('@azure/functions')
+const BoxCastSDK = require('@boxcast/boxcast-sdk-js/node')
+
+const config = {
+    boxcastApiEndpoint: 'https://rest.boxcast.com'
+}
+
+const getBroadcast = async channelId => {
+    if ( ! channelId) return false
+
+    const { api } = new BoxCastSDK()
+    const result = await api.broadcasts.list(channelId, {
+        q: 'timeframe:relevant',
+        s: '-starts_at',
+        l: 1
+    })
+
+    if ( ! result.data[0]) return false
+
+    const { id, name, starts_at, stops_at, timeframe } = result.data[0]
+
+    return { id, name, starts_at, stops_at, timeframe }
+}
+
+app.http('HttpTrigger', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`)
+
+        // const name = request.query.get('name') || await request.text() || 'world';
+        const channelId = request.query.get('channelId')
+        const details = await getBroadcast(channelId)
+
+        return {
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                status: 'success',
+                channelId,
+                details
+            })
+        }
+    }
+})
